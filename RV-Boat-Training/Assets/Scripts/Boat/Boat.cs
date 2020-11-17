a309bf8806d8474612d3eacc0a5d2d2e59a5a29c
Augusto Pedron <progettograficapolito@gmail.com>
2020-11-17 17:17:03 +0100

    aggiunta una base per la gestione di carburante e ancoraggio

--- RV-Boat-Training/Assets/Scripts/Boat/Boat.cs
+++ RV-Boat-Training/Assets/Scripts/Boat/Boat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using System.Collections;
 
 namespace BoatAttack
 {
@@ -11,73 +12,176 @@ namespace BoatAttack
         public float steeringSpeed = 5f;
         public float accelerationSpeed = 5f;
         public float decelerationSpeed = 0.1f;
+        private bool engineRunning = true;
+        public float anchorProbability = 30f;
+        public float anchorProbabilityIncrement = 5f;
+        public bool anchorSet = false;
+        public bool anchorStart = false;
 
         private void Awake()
         {
             TryGetComponent(out engine.RB);
         }
 
+        private void OnEnable()
+        {
+            BoatEventManager.StartListening("emptyTank", StopEngine);
+        }
+
+        private void OnDisable()
+        {
+            BoatEventManager.StopListening("emptyTank", StopEngine);
+        }
+
         private void Update()
         {
-            if (Input.GetKey(KeyCode.A))
+            if (!anchorSet)
             {
-                steering -= steeringSpeed * Time.deltaTime;
-                steering = Mathf.Clamp(steering, -1f, 0f);
-                engine.Turn(steering);
+                if (Input.GetKey(KeyCode.A))
+                {
+                    if (engineRunning)
+                    {
+                        UpdateSteering(-1f);
+                    }
+                    else
+                    {
+                        steering = ResidualTurningForce(steering, -decelerationSpeed);
+                    }
+                }
+
+                if (steering < 0f && !Input.GetKey(KeyCode.A))
+                {
+                    steering = ResidualTurningForce(steering, -decelerationSpeed);
+                }
+
+                if (Input.GetKey(KeyCode.D))
+                {
+                    if (engineRunning)
+                    {
+                        UpdateSteering(1f);
+                    }
+                    else
+                    {
+                        steering = ResidualTurningForce(steering, decelerationSpeed);
+                    }
+                }
+
+                if (steering > 0f && !Input.GetKey(KeyCode.D))
+                {
+                    steering = ResidualTurningForce(steering, decelerationSpeed);
+                }
+
+                if (Input.GetKey(KeyCode.W))
+                {
+                    if (engineRunning)
+                    {
+                        UpdateAcceleration(1f);
+                    }
+                    else
+                    {
+                        acceleration = ResidualForwardForce(acceleration, decelerationSpeed);
+                    }
+                }
+
+                if (acceleration > 0f && !Input.GetKey(KeyCode.W))
+                {
+                    acceleration = ResidualForwardForce(acceleration, decelerationSpeed);
+                }
+
+                if (Input.GetKey(KeyCode.S))
+                {
+                    if (engineRunning)
+                    {
+                        UpdateAcceleration(-1f);
+                    }
+                    else
+                    {
+                        acceleration = ResidualForwardForce(acceleration, -decelerationSpeed);
+                    }
+                }
+
+                if (acceleration < 0f && !Input.GetKey(KeyCode.S))
+                {
+                    acceleration = ResidualForwardForce(acceleration, -decelerationSpeed);
+                }
+
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    anchorStart = true;
+                }
             }
-            if (steering < 0f && !Input.GetKey(KeyCode.A))
+
+            if (anchorStart)
             {
-                if (steering <= decelerationSpeed * 4 * Time.deltaTime)
-                    steering += decelerationSpeed * 4 * Time.deltaTime;
+                float prob = UnityEngine.Random.Range(0f, 100f);
+                if (prob <= anchorProbability)
+                {
+                    anchorSet = true;
+                    anchorStart = false;
+                }
+
                 else
-                    steering = 0f;
-                engine.Turn(steering);
+                    anchorProbability += anchorProbabilityIncrement * Time.deltaTime;
             }
-            if (Input.GetKey(KeyCode.D))
+
+            if (anchorSet)
             {
-                steering += steeringSpeed * Time.deltaTime;
-                steering = Mathf.Clamp01(steering);
-                engine.Turn(steering);
-            }
-            if (steering > 0f && !Input.GetKey(KeyCode.D)){
-                if (steering >= decelerationSpeed * 4 * Time.deltaTime)
-                    steering -= decelerationSpeed * 4 * Time.deltaTime;
-                else
-                    steering = 0f;
-                engine.Turn(steering);
-            }
-            if (Input.GetKey(KeyCode.W))
-            {
-                acceleration += accelerationSpeed * Input.mouseScrollDelta.y * Time.deltaTime;
-                acceleration = Mathf.Clamp01(acceleration);
-                engine.Accelerate(acceleration);
-            }
-            if (acceleration > 0f && !Input.GetKey(KeyCode.W))
-            {
-                if (acceleration >= decelerationSpeed * Time.deltaTime)
-                    acceleration -= decelerationSpeed * Time.deltaTime;
-                else
-                    acceleration = 0f;
-                engine.Accelerate(acceleration);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                acceleration -= accelerationSpeed * Input.mouseScrollDelta.y * Time.deltaTime;
-                acceleration = Mathf.Clamp(acceleration, -1f, 0f);
-                engine.Accelerate(acceleration);
-            }
-            if (acceleration < 0f && !Input.GetKey(KeyCode.S))
-            {
-                if (acceleration <= decelerationSpeed * Time.deltaTime)
-                    acceleration += decelerationSpeed * Time.deltaTime;
-                else
-                    acceleration = 0f;
-                engine.Accelerate(acceleration);
+                if (Input.GetKeyDown(KeyCode.F)) anchorSet = false;
+
+                if(acceleration > 0f)
+                {
+                    acceleration = ResidualForwardForce(acceleration, 4f * decelerationSpeed);
+                }
+
+                if(acceleration < 0f)
+                {
+                    acceleration = ResidualForwardForce(acceleration, -4f * decelerationSpeed);
+                }
             }
         }
 
         private void LateUpdate()
         {
+        }
+
+        private void StopEngine()
+        {
+            BoatEventManager.StopListening("emptyTank", StopEngine);
+            engineRunning = false;
+        }
+
+        private void UpdateAcceleration(float forward)
+        {
+            acceleration += accelerationSpeed * Input.mouseScrollDelta.y * Time.deltaTime * forward;
+            acceleration = Mathf.Clamp(acceleration, -1f, 1f);
+            engine.Accelerate(acceleration);
+        }
+
+        private float ResidualForwardForce(float acceleration, float decelerationSpeed)
+        {
+            if (acceleration >= decelerationSpeed * Time.deltaTime)
+                acceleration -= decelerationSpeed * Time.deltaTime;
+            else
+                acceleration = 0f;
+            engine.AccelerateNoFuel(acceleration);
+            return acceleration;
+        }
+
+        private void UpdateSteering(float right)
+        {
+            steering += steeringSpeed * Time.deltaTime * right;
+            steering = Mathf.Clamp(steering, -1f, 1f);
+            engine.Turn(steering);
+        }
+
+        private float ResidualTurningForce(float steering, float decelerationSpeed)
+        {
+            if (steering >= decelerationSpeed * 4 * Time.deltaTime)
+                steering -= decelerationSpeed * 4 * Time.deltaTime;
+            else
+                steering = 0f;
+            engine.TurnNoFuel(steering);
+            return steering;
         }
     }
 }
--- RV-Boat-Training/Assets/Scripts/Boat/Engine.cs
+++ RV-Boat-Training/Assets/Scripts/Boat/Engine.cs
@@ -17,6 +17,9 @@ namespace BoatAttack
         //engine stats
         public float steeringTorque = 5f;
         public float horsePower = 18f;
+        public float fuel = 100f;
+        public float tankCapacity = 200f;
+        public float fuelConsumptionPerHour = 30f;
         private NativeArray<float3> _point; // engine submerged check
         private float3[] _heights = new float3[1]; // engine submerged check
         private float3[] _normals = new float3[1]; // engine submerged check
@@ -27,18 +30,21 @@ namespace BoatAttack
         private Vector3 _engineDir;
         private float _turnVel;
         private float _currentAngle;
+        private bool updateFuel = false;
 
         private void Awake()
         {
-			//if(engineSound)
-			//	engineSound.time = UnityEngine.Random.Range(0f, engineSound.clip.length); // randomly start the engine sound
+            //if(engineSound)
+            //	engineSound.time = UnityEngine.Random.Range(0f, engineSound.clip.length); // randomly start the engine sound
 
-			//if(waterSound)
-			//	waterSound.time = UnityEngine.Random.Range(0f, waterSound.clip.length); // randomly start the water sound
+            //if(waterSound)
+            //	waterSound.time = UnityEngine.Random.Range(0f, waterSound.clip.length); // randomly start the water sound
 
             _guid = GetInstanceID(); // Get the engines GUID for the buoyancy system
             _point = new NativeArray<float3>(1, Allocator.Persistent);
         }
+
+        private void OnEnable() { fuel = fuel <= tankCapacity ? fuel : tankCapacity; }
 
         private void FixedUpdate()
         {
@@ -50,18 +56,43 @@ namespace BoatAttack
             GerstnerWavesJobs.UpdateSamplePoints(ref _point, _guid);
             GerstnerWavesJobs.GetData(_guid, ref _heights, ref _normals);
             _yHeight = _heights[0].y - _point[0].y;
+
+        }
+
+        private void LateUpdate()
+        {
+            if (updateFuel)
+            {
+                fuel = fuel >= fuelConsumptionPerHour / 3600 * Time.fixedDeltaTime ? fuel - (fuelConsumptionPerHour / 3600 * Time.fixedDeltaTime) : 0f;
+                updateFuel = false;
+                if (fuel == 0f) BoatEventManager.TriggerEvent("emptyTank");
+            }
         }
 
         private void OnDisable()
         {
             _point.Dispose();
         }
-        
+
         /// <summary>
         /// Controls the acceleration of the boat
         /// </summary>
         /// <param name="modifier">Acceleration modifier, adds force in the 0-1 range</param>
         public void Accelerate(float modifier)
+        {
+            if (_yHeight > -0.1f && fuel > 0f) // if the engine is deeper than 0.1
+            {
+                //modifier = Mathf.Clamp(modifier, -10f, 10f); // clamp for reasonable values
+                var forward = RB.transform.forward;
+                forward.y = 0f;
+                forward.Normalize();
+                RB.AddForce(horsePower * modifier * forward, ForceMode.Acceleration); // add force forward based on input and horsepower
+                RB.AddRelativeTorque(-Vector3.right * modifier * 0.01f, ForceMode.Acceleration);
+                updateFuel = true;
+            }
+        }
+
+        public void AccelerateNoFuel(float modifier)
         {
             if (_yHeight > -0.1f) // if the engine is deeper than 0.1
             {
@@ -80,20 +111,44 @@ namespace BoatAttack
         /// <param name="modifier">Steering modifier, positive for right, negative for negative</param>
         public void Turn(float modifier)
         {
+            if (fuel > 0f)
+            {
+                if (_yHeight > -0.1f) // if the engine is deeper than 0.1
+                {
+                    //modifier = Mathf.Clamp(modifier, -1f, 1f); // clamp for reasonable values
+                    RB.AddRelativeTorque(new Vector3(0f, steeringTorque, -steeringTorque * 0.5f) * modifier, ForceMode.Acceleration); // add torque based on input and torque amount
+                    updateFuel = true;
+                }
+
+                _currentAngle = Mathf.SmoothDampAngle(_currentAngle,
+                    60f * -modifier,
+                    ref _turnVel,
+                    0.5f,
+                    10f,
+                    Time.fixedTime);
+                transform.localEulerAngles = new Vector3(0f, _currentAngle, 0f);
+            }
+        }
+
+        public void TurnNoFuel(float modifier)
+        {
             if (_yHeight > -0.1f) // if the engine is deeper than 0.1
             {
                 //modifier = Mathf.Clamp(modifier, -1f, 1f); // clamp for reasonable values
                 RB.AddRelativeTorque(new Vector3(0f, steeringTorque, -steeringTorque * 0.5f) * modifier, ForceMode.Acceleration); // add torque based on input and torque amount
             }
 
-            _currentAngle = Mathf.SmoothDampAngle(_currentAngle, 
-                60f * -modifier, 
-                ref _turnVel, 
-                0.5f, 
+            _currentAngle = Mathf.SmoothDampAngle(_currentAngle,
+                60f * -modifier,
+                ref _turnVel,
+                0.5f,
                 10f,
                 Time.fixedTime);
             transform.localEulerAngles = new Vector3(0f, _currentAngle, 0f);
+
         }
+
+        public void SetFuel(float fuel) { this.fuel = fuel <= tankCapacity ? fuel : tankCapacity; }
 
         // Draw some helper gizmos
         private void OnDrawGizmosSelected()
@@ -102,5 +157,5 @@ namespace BoatAttack
             Gizmos.matrix = transform.localToWorldMatrix;
             Gizmos.DrawCube(enginePosition, new Vector3(0.1f, 0.2f, 0.3f)); // Draw teh engine position with sphere
         }
-	}
+    }
 }
