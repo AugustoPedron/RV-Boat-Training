028724472027e8f953984b4d4129f3f52796b95b
Augusto Pedron <progettograficapolito@gmail.com>
2020-12-11 16:39:52 +0100

    Creazione UI - da finire

---
+++ RV-Boat-Training/Assets/Scripts/UI/PanelFader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelFader : MonoBehaviour
+{
+    public float duration = 0.4f;
+    public CanvasGroup cvnGroup;
+
+    public void Fade(CanvasGroup nextPanel)
+    {
+        StartCoroutine(DoFade(nextPanel, cvnGroup.alpha, 0));
+        cvnGroup.blocksRaycasts = false;
+        nextPanel.blocksRaycasts = true;
+    }
+
+    private IEnumerator DoFade(CanvasGroup nextPanel, float start, float end)
+    {
+        float counter = 0f;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            cvnGroup.alpha = Mathf.Lerp(start, end, counter / duration);
+
+            yield return null;
+        }
+
+        counter = 0f;
+
+        float nextPanelStart = nextPanel.alpha;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            nextPanel.alpha = Mathf.Lerp(nextPanelStart, 1, counter / duration);
+
+            yield return null;
+        }
+    }
+}
