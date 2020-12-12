3697285c0ad1c2bc7fdeb865e0730d53a72554c0
Augusto Pedron <progettograficapolito@gmail.com>
2020-12-12 17:08:19 +0100

    ui iniziale funzionante

--- RV-Boat-Training/Assets/Scripts/UI/PanelFader.cs
+++ RV-Boat-Training/Assets/Scripts/UI/PanelFader.cs
@@ -6,11 +6,32 @@ public class PanelFader : MonoBehaviour
 {
     public float duration = 0.4f;
     public CanvasGroup cvnGroup;
+    [SerializeField]
+    private UIManager uiManager;
 
-    public void Fade(CanvasGroup nextPanel)
+    public void FadeNext(CanvasGroup nextPanel)
     {
         StartCoroutine(DoFade(nextPanel, cvnGroup.alpha, 0));
         cvnGroup.blocksRaycasts = false;
+        nextPanel.blocksRaycasts = true;
+    }
+
+    public void FadeNext()
+    {
+        CanvasGroup activePanel = uiManager.GetActivePanel();
+        CanvasGroup nextPanel = uiManager.GetNextPanel();
+        StartCoroutine(DoFade(activePanel, nextPanel, activePanel.alpha, 0));
+        activePanel.blocksRaycasts = false;
+        nextPanel.blocksRaycasts = true;
+    }
+
+    public void FadePrevious()
+    {
+        CanvasGroup activePanel = uiManager.GetActivePanel();
+        CanvasGroup nextPanel = uiManager.GetPreviousPanel();
+        if (uiManager.GetActivePanelNum() == 0) activePanel = cvnGroup;
+        StartCoroutine(DoFade(activePanel, nextPanel, activePanel.alpha, 0));
+        activePanel.blocksRaycasts = false;
         nextPanel.blocksRaycasts = true;
     }
 
@@ -22,6 +43,31 @@ public class PanelFader : MonoBehaviour
         {
             counter += Time.deltaTime;
             cvnGroup.alpha = Mathf.Lerp(start, end, counter / duration);
+
+            yield return null;
+        }
+
+        counter = 0f;
+
+        float nextPanelStart = nextPanel.alpha;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            nextPanel.alpha = Mathf.Lerp(nextPanelStart, 1, counter / duration);
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator DoFade(CanvasGroup activePanel, CanvasGroup nextPanel, float start, float end)
+    {
+        float counter = 0f;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            activePanel.alpha = Mathf.Lerp(start, end, counter / duration);
 
             yield return null;
         }
---
+++ RV-Boat-Training/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public List<CanvasGroup> canvasGroups;
+    private int activePanel = 1;
+
+    public CanvasGroup GetPreviousPanel()
+    {
+        if (activePanel > 0)
+            return canvasGroups[--activePanel];
+        else
+            return null;
+    }
+
+    public CanvasGroup GetNextPanel()
+    {
+        if (activePanel < canvasGroups.Count - 1)
+            return canvasGroups[++activePanel];
+        else
+            return null;
+    }
+
+    public CanvasGroup GetActivePanel()
+    {
+        return canvasGroups[activePanel];
+    }
+
+    public int GetActivePanelNum()
+    {
+        return activePanel;
+    }
+}
