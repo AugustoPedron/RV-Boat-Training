76e4e7883ecca9f6266db6cee3edae8a3f206df0
andreal <andrea.lucaroni@live.com>
2020-12-13 16:17:26 +0100

    Seconda barca
    Creazione di una seconda barca di prova

---
+++ RV-Boat-Training/Assets/Scripts/NpcBoat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcBoat : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float velocity = 1f;
+    public GameObject playerboat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerboat = GameObject.Find("Boat");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (playerboat.transform.position.z > 1000) Avanza();
+
+    }
+    public void Avanza()
+    {
+var forward = rb.transform.forward;
+        forward.y = 0f;
+        forward.Normalize();
+        rb.AddForce(velocity * forward, ForceMode.Acceleration);
+    }
+}
