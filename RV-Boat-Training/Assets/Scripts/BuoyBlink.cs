a092490c7ca54f16d3a22fb474f1302cc73eb607
Augusto Pedron <progettograficapolito@gmail.com>
2020-12-18 18:43:57 +0100

    colori boe e lampeggio luce boa

---
+++ RV-Boat-Training/Assets/Scripts/BuoyBlink.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuoyBlink : MonoBehaviour
+{
+    public Material material;
+    private bool on = true;
+
+    private void OnEnable()
+    {
+        StartCoroutine(Blink());
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            if (on)
+            {
+                material.DisableKeyword("_EMISSION");
+                on = false;
+            }
+            else
+            {
+                material.EnableKeyword("_EMISSION");
+                on = true;
+            }
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
