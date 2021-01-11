92dbc9921175a5308228225bb297b5d72efd4c6d
Augusto Pedron <progettograficapolito@gmail.com>
2021-01-11 14:57:30 +0100

    aggiunto caricamento e transizione tra scene

--- RV-Boat-Training/Assets/Scripts/UI/LoadingScene.cs
+++ RV-Boat-Training/Assets/Scripts/UI/LoadingScene.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadingScene : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Animator animator;
+    public Image blackImage;
+
+    private bool activeScene = false;
+
     void Start()
     {
-        
+        StartCoroutine(LoadScene());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadScene()
     {
-        
+        yield return new WaitForSeconds(2);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("SampleScene");
+        asyncOperation.allowSceneActivation = false;
+
+        while (!asyncOperation.isDone)
+        {
+            //Output the current progress
+            //m_Text.text = "Loading progress: " + (asyncOperation.progress * 100) + "%";
+
+            // Check if the load has finished
+            if (asyncOperation.progress >= 0.9f)
+            {
+                //Change the Text to show the Scene is ready
+                //m_Text.text = "Press the space bar to continue";
+                //Wait to you press the space key to activate the Scene
+                if (activeScene)
+                {
+                    //Activate the Scene
+                    animator.SetBool("Fade", true);
+                    yield return new WaitUntil(() => blackImage.color.a == 1);
+                    asyncOperation.allowSceneActivation = true;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    public void SetActiveScene()
+    {
+        activeScene = true;
     }
 }
