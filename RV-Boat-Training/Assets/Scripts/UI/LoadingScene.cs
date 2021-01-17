b4737af925adf5a1e5af244321f650edaface7f5
Augusto Pedron <augusto.pedron@gmail.com>
2021-01-17 18:42:16 +0100

    menu principale finito

--- RV-Boat-Training/Assets/Scripts/UI/LoadingScene.cs
+++ RV-Boat-Training/Assets/Scripts/UI/LoadingScene.cs
@@ -8,47 +8,67 @@ public class LoadingScene : MonoBehaviour
 {
     public Animator animator;
     public Image blackImage;
+    public Slider loadingBar;
+    public GameObject loadingBarObj;
+    public GameObject loadingText;
+    private AsyncOperation loading;
 
     private bool activeScene = false;
 
     void Start()
     {
-        StartCoroutine(LoadScene());
+         //StartCoroutine(LoadScene());
     }
 
-    IEnumerator LoadScene()
+    //IEnumerator LoadScene()
+    //{
+    //    yield return new WaitForSeconds(2);
+
+    //    AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("SampleScene", LoadSceneMode.Single);
+    //    asyncOperation.allowSceneActivation = false;
+
+    //    while (!asyncOperation.isDone)
+    //    {
+    //        //Output the current progress
+    //        //m_Text.text = "Loading progress: " + (asyncOperation.progress * 100) + "%";
+
+    //        // Check if the load has finished
+    //        if (asyncOperation.progress >= 0.9f)
+    //        {
+    //            //Change the Text to show the Scene is ready
+    //            //m_Text.text = "Press the space bar to continue";
+    //            //Wait to you press the space key to activate the Scene
+    //            if (activeScene)
+    //            {
+    //                //Activate the Scene
+    //                animator.SetBool("Fade", true);
+    //                yield return new WaitUntil(() => blackImage.color.a == 1);
+    //                asyncOperation.allowSceneActivation = true;
+    //            }
+    //        }
+
+    //        yield return null;
+    //    }
+    //}
+
+    private void Update()
     {
-        yield return new WaitForSeconds(2);
-
-        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("SampleScene", LoadSceneMode.Additive);
-        asyncOperation.allowSceneActivation = false;
-
-        while (!asyncOperation.isDone)
+        if (loading != null)
         {
-            //Output the current progress
-            //m_Text.text = "Loading progress: " + (asyncOperation.progress * 100) + "%";
-
-            // Check if the load has finished
-            if (asyncOperation.progress >= 0.9f)
-            {
-                //Change the Text to show the Scene is ready
-                //m_Text.text = "Press the space bar to continue";
-                //Wait to you press the space key to activate the Scene
-                if (activeScene)
-                {
-                    //Activate the Scene
-                    animator.SetBool("Fade", true);
-                    yield return new WaitUntil(() => blackImage.color.a == 1);
-                    asyncOperation.allowSceneActivation = true;
-                }
-            }
-
-            yield return null;
+            float progressValue = Mathf.Clamp01(loading.progress / 0.9f);
+            loadingBar.value = Mathf.Clamp01(loading.progress / 0.9f);
         }
     }
 
-    public void SetActiveScene()
+    public void LoadScene()
     {
-        activeScene = true;
+        animator.SetBool("Fade", true);
+        loadingText.SetActive(true);
+        loadingBarObj.SetActive(true);
+    }
+
+    public void StartLoading()
+    {
+        loading = SceneManager.LoadSceneAsync("SampleScene", LoadSceneMode.Single);
     }
 }
