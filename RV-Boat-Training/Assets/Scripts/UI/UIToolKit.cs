d99f840c936f5d4041bcc79ad3ff5ea5ce9e3605
Augusto Pedron <augusto.pedron@gmail.com>
2021-01-26 17:34:10 +0100

    stuff

--- RV-Boat-Training/Assets/Scripts/UI/UIToolKit.cs
+++ RV-Boat-Training/Assets/Scripts/UI/UIToolKit.cs
@@ -7,38 +7,46 @@ using UnityEngine.UI;
 public class UIToolKit : MonoBehaviour
 {
     public Button referenceButton;
-
-    public TMP_FontAsset fontForText;
+    public Color color;
+    public Color normalColor;
+    public Color highlightenedColor;
+    public Color pressedColor;
+    public TMP_FontAsset font;
     public Canvas menu;
 
     public void ChangeButtonsStyle()
     {
         if (referenceButton != null && menu != null)
         {
-            ColorBlock cb = referenceButton.colors;
-            Color c = referenceButton.GetComponent<Image>().color;
-            TMP_FontAsset f = referenceButton.GetComponentInChildren<TMP_Text>().font;
+            //ColorBlock cb = referenceButton.colors;
+            ColorBlock cb = ColorBlock.defaultColorBlock;
+            cb.normalColor = normalColor;
+            cb.highlightedColor = highlightenedColor;
+            cb.pressedColor = pressedColor;
+
+            //Color c = referenceButton.GetComponent<Image>().color;
+            //TMP_FontAsset f = referenceButton.GetComponentInChildren<TMP_Text>().font;
             Button[] buttons = menu.GetComponentsInChildren<Button>();
 
             for (int i = 0; i < buttons.Length; i++)
             {
                 buttons[i].colors = cb;
-                buttons[i].GetComponent<Image>().color = c;
-                buttons[i].GetComponentInChildren<TMP_Text>().font = f;
+                buttons[i].GetComponent<Image>().color = color;
+                buttons[i].GetComponentInChildren<TMP_Text>().font = font;
             }
         }
     }
 
     public void ChangeTextsStyle()
     {
-        if (fontForText != null && menu != null)
+        if (font != null && menu != null)
         {
             TMP_Text[] texts = menu.GetComponentsInChildren<TMP_Text>();
             for (int i = 0; i < texts.Length; i++)
             {
                 if (texts[i].GetComponentInParent<Button>() == null)
                 {
-                    texts[i].font = fontForText;
+                    texts[i].font = font;
                 }
             }
         }
